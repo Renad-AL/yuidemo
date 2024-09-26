c4225a26202d44a3896902120bae7866876b3c4d
Renad <uniirena@gmail.com>
2024-09-26 16:51:53 +0300

    Initial Commit

---
+++ yuidemo/yuidemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  yuidemoApp.swift
+//  yuidemo
+//
+//  Created by Renad Alyousef on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct yuidemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ yuidemoTests/yuidemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  yuidemoTests.swift
+//  yuidemoTests
+//
+//  Created by Renad Alyousef on 23/03/1446 AH.
+//
+
+import Testing
+@testable import yuidemo
+
+struct yuidemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
